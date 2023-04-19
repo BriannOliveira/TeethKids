2ddeffaef5096ce420014bb438166d4cdb9e8c8e
Briann <brianngomess@gmail.com>
2023-04-19 20:35:23 -0300

    Tentativa da criação da função de tirar selfie

--- app/src/main/java/br/com/minhaempresa/teethkids/LoginFragment.kt
+++ app/src/main/java/br/com/minhaempresa/teethkids/LoginFragment.kt
@@ -10,10 +10,23 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.provider.MediaStore
 import android.app.Activity.RESULT_OK
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore.Audio.Media
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.core.graphics.createBitmap
 import br.com.minhaempresa.teethkids.databinding.FragmentLoginBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class LoginFragment : Fragment() {
@@ -22,6 +35,7 @@ class LoginFragment : Fragment() {
     private val binding get() = _binding!!
     private val auth = FirebaseAuth.getInstance()
 
+    //Na inicialização da tela
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,14 +45,24 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    //Na tela criada
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
         //Configuração para a imagem ter função para tirar selfie
         binding.imgUsuario.setOnClickListener(){
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+
+            //Condição para ver se o usuário permitiu o acesso do aplicativo à camêra,
+            //Caso checkPermission não tem um valor ou for falso, ele pede a permissão
+            if(!checkPermission() || checkPermission()==false){
+                requestPermission()
+            }
+
+            //Caso true, ele aciona a função para tirar foto com um argumento que informa aonde a foto deve ser posta
+            if (!checkPermission()==true){
+                takePhoto(binding.imgUsuario)
+            }
         }
 
         //Botão para ir para o terceiro fragmento do login
@@ -74,12 +98,30 @@ class LoginFragment : Fragment() {
 
     }
 
+    //Função quando o usuário inicia uma ação de permissão entre o SO e o aplicativo
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CAMERA_PERMISSION_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //Permissão concedida
+            }else {
+                Toast.makeText(requireContext(),"Permissão de camêra negada", Toast.LENGTH_SHORT).show()
+                    //Permissão negada
+                }
+            }
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.imgUsuario.setImageBitmap(imageBitmap)
+            val bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, imageUri)
+            binding.imgUsuario.setImageBitmap(bitmap)
         }
     }
 
@@ -88,7 +130,58 @@ class LoginFragment : Fragment() {
         _binding = null
     }
 
+    //Função que checa se o usuário permitiu o acesso à câmera e retorna um Boolean
+    private fun checkPermission(): Boolean{
+        return ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    //Função que pede permissão do acesso à câmera para o aplicativo
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(android.Manifest.permission.CAMERA),
+            REQUEST_CAMERA_PERMISSION_CODE
+        )
+    }
+
+    //Função para tirar foto e substituir a foto tirada por alguma ImageView
+    private fun takePhoto(view: View){
+        //Cria uma nova intenção do tipo "Media.Store.ACTION_IMAGE_CAPTURE" para iniciar a camêra do dispositivo
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePhoto ->
+            takePhoto.resolveActivity(requireActivity().packageManager)?.also {
+                val photoFile : File? = try {
+                    createImageFile()               //Cria um novo arquivo para armazenar a imagem capturada pela câmera.
+                } catch (ex: IOException){
+                    null                            //Erro ao criar o arquivo
+                }
+
+                photoFile?.also {
+                    imageUri = FileProvider.getUriForFile(requireContext(), "com.example.android.fileprovider", it)
+                    takePhoto.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+                    startActivityForResult(takePhoto, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    //Função para criar um arquivo em que a imagem capturada possa ser armazenada
+    @Throws(IOException::class)
+    private fun createImageFile(): File{
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir                  //Esse arquivo será salvo no diretório específico do aplicativo para armazenamento de imagens.
+        ).apply {
+            val currentPhotoPath = absolutePath
+        }
+    }
+
+    //Companion object permite que você defina propriedades e métodos em uma classe
     companion object{
         private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val REQUEST_CAMERA_PERMISSION_CODE = 100
+        private lateinit var imageUri : Uri
     }
 }
